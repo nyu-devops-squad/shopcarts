ee5350d2fba43cab22288666b9f7f388e3524f51
Qingyang Li <liqingyang1602@163.com>
2021-12-08 18:15:44 +0800

    modify config.py

--- config.py
+++ config.py
@@ -1,21 +1,3 @@
-"""
-Global Configuration for Application
-"""
-import os
-import json
-
-# Get configuration from environment
-DATABASE_URI = os.getenv(
-    "DATABASE_URI", "postgres://postgres:postgres@localhost:5432/postgres"
-)
-
-if 'VCAP_SERVICES' in os.environ:
-    vcap = json.loads(os.environ['VCAP_SERVICES'])
-    DATABASE_URI = vcap['user-provided'][0]['credentials']['url']
-    
-# Configure SQLAlchemy
-SQLALCHEMY_DATABASE_URI = DATABASE_URI
-SQLALCHEMY_TRACK_MODIFICATIONS = False
-
-# Secret for session management
-SECRET_KEY = os.getenv("SECRET_KEY", "s3cr3t-key-shhhh")
+import logging
+SECRET_KEY = 'secret-for-dev'
+LOGGING_LEVEL = logging.INFO
