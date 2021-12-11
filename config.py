9dc83ac105b16ccbdfdb1391f49bf2986aacaa16
Qingyang Li <liqingyang1602@163.com>
2021-12-11 10:45:42 +0800

    mod2: config.py

--- config.py
+++ config.py
@@ -6,5 +6,9 @@ if 'VCAP_SERVICES' in os.environ:
     vcap = json.loads(os.environ['VCAP_SERVICES'])
     DATABASE_URI = vcap['user-provided'][0]['credentials']['url']
 
+# Configure SQLAlchemy
+SQLALCHEMY_DATABASE_URI = DATABASE_URI
+SQLALCHEMY_TRACK_MODIFICATIONS = False
+
 SECRET_KEY = 'secret-for-dev'
 LOGGING_LEVEL = logging.INFO
