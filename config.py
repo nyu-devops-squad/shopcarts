aa69745fcf5232289eb7fbb6cd7f997ee418cfb8
Qingyang Li <liqingyang1602@163.com>
2021-12-11 10:08:46 +0800

    modify config.py

--- config.py
+++ config.py
@@ -1,3 +1,10 @@
+import os
+import json
 import logging
+
+if 'VCAP_SERVICES' in os.environ:
+    vcap = json.loads(os.environ['VCAP_SERVICES'])
+    DATABASE_URI = vcap['user-provided'][0]['credentials']['url']
+
 SECRET_KEY = 'secret-for-dev'
 LOGGING_LEVEL = logging.INFO
