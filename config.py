6ecfc0964ac8ec0d8ccba5302c756b7121b3df0b
Qingyang Li <liqingyang1602@163.com>
2021-12-11 10:58:08 +0800

    mod3: config.py

--- config.py
+++ config.py
@@ -1,13 +1,15 @@
 import os
 import json
 import logging
+from app.vcap_services import get_database_uri
 
 if 'VCAP_SERVICES' in os.environ:
     vcap = json.loads(os.environ['VCAP_SERVICES'])
     DATABASE_URI = vcap['user-provided'][0]['credentials']['url']
+    print("Using VCAP_SERVICES")  # for debug purpose
 
 # Configure SQLAlchemy
-SQLALCHEMY_DATABASE_URI = DATABASE_URI
+SQLALCHEMY_DATABASE_URI = get_database_uri()
 SQLALCHEMY_TRACK_MODIFICATIONS = False
 
 SECRET_KEY = 'secret-for-dev'
